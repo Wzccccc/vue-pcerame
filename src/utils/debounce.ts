7438cfc139cac7022a98e14438c00b5b94490974
w w <ww@wdeMacBook-Pro.local>
2023-03-08 23:50:20 +0800

    fix: 修复防抖函数多次点击无法触发关闭问题

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -7,48 +7,52 @@
  * @param finishCallback 结束时的回调函数
  * @returns 节流过的函数(可取消执行防抖函数)
  */
-export function debounce<T = any, R = any>(fn: (...args: T[]) => R, delay: number = 500, immediate: boolean = false, beginCallBack: Function = () => { }, finishCallback: Function = () => { }) {
-    let timer: NodeJS.Timeout | null = null
-    let isInvoke = false
-    const _debounce = function (...args: T[]) {
-        return new Promise(async (resolve, reject) => {
-            try {
-                beginCallBack()
-                // 取消上一次的定时器
-                if (timer) {
-                    clearTimeout(timer)
-                    finishCallback()
-                }
-                if (immediate && !isInvoke) {
-                    // 立即执行
-                    const result = await fn.apply(null, args)
-                    resolve(result)
-                    isInvoke = true
-                    finishCallback()
-                } else {
-                    // 延迟执行
-                    timer = setTimeout(async () => {
-                        const result = await fn.apply(null, args)
-                        resolve(result)
-                        isInvoke = false
-                        timer = null
-                        finishCallback()
-                    }, delay)
-                }
-            } catch (err) {
-                reject(err)
-            }
-        })
-    }
-    // 取消功能
-    _debounce.cancel = function () {
-        if (timer) {
-            clearTimeout(timer)
-            finishCallback()
-        }
-        timer = null
-        isInvoke = false
-    }
-    return _debounce
+export function debounce<T = any, R = any>(
+	fn: (...args: T[]) => R,
+	delay: number = 500,
+	immediate: boolean = false,
+	beginCallBack: Function = () => {},
+	finishCallback: Function = () => {}
+) {
+	let timer: NodeJS.Timeout | null = null;
+	let isInvoke = false;
+	const _debounce = function (...args: T[]) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				beginCallBack();
+				// 取消上一次的定时器
+				if (timer) {
+					clearTimeout(timer);
+				}
+				if (immediate && !isInvoke) {
+					// 立即执行
+					const result = await fn.apply(null, args);
+					resolve(result);
+					isInvoke = true;
+					finishCallback();
+				} else {
+					// 延迟执行
+					timer = setTimeout(async () => {
+						const result = await fn.apply(null, args);
+						resolve(result);
+						isInvoke = false;
+						timer = null;
+						finishCallback();
+					}, delay);
+				}
+			} catch (err) {
+				reject(err);
+			}
+		});
+	};
+	// 取消功能
+	_debounce.cancel = function () {
+		if (timer) {
+			clearTimeout(timer);
+			finishCallback();
+		}
+		timer = null;
+		isInvoke = false;
+	};
+	return _debounce;
 }
-
