82dd20ac431aaa93c591f5b9aa8bcaca9a7d789f
Symbol <1783176286@qq.com>
2023-05-20 14:19:55 +0800

    build: 打包优化

--- build/getEnv.ts
+++ build/getEnv.ts
@@ -4,7 +4,9 @@ export function convertEnvType(envConfig: Recordable): ViteEnv {
 	let realEnv: any = { ...envConfig };
 	for (const key of Object.keys(envConfig)) {
 		key === "VITE_APP_PORT" && (realEnv[key] = Number(realEnv[key]));
-		["VITE_APP_PROXY", "VITE_APP_REPORT"].includes(key) && (realEnv[key] = Boolean(realEnv[key]));
+		["VITE_APP_PROXY", "VITE_APP_REPORT", "VITE_APP_DROP_CONSOLE", "VITE_APP_BUILD_COMPRESS_DELETE_ORIGIN_FILE"].includes(key) &&
+			(realEnv[key] = Boolean(realEnv[key]));
 	}
+	console.log(realEnv);
 	return realEnv;
 }
