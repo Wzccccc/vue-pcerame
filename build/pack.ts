33e2efbef36d9ab229a0a966eddb53dedc78b435
Symbol <1783176286@qq.com>
2023-05-21 17:42:19 +0800

    test: 测试打包预览文件不提交

--- build/pack.ts
+++ build/pack.ts
@@ -7,32 +7,32 @@ import viteCompression from "vite-plugin-compression"; //文件压缩
  * @returns (PluginOption | PluginOption[])[]
  */
 export const createCompress = (viteEnv: ViteEnv): (PluginOption | PluginOption[])[] => {
-	const { VITE_APP_BUILD_COMPRESS, VITE_APP_BUILD_COMPRESS_DELETE_ORIGIN_FILE } = viteEnv;
-	const compressList = VITE_APP_BUILD_COMPRESS.split(",");
-	const plugins: PluginOption[] = [];
-	for (const law of compressList) {
-		law === "gzip" &&
-			plugins.push(
-				viteCompression({
-					verbose: true, // 是否在控制台中输出压缩结果
-					disable: false,
-					threshold: 1024, // 如果体积大于阈值，将被压缩，单位为b，体积过小时请不要压缩，以免适得其反
-					algorithm: "gzip", // 压缩算法
-					ext: ".gz",
-					deleteOriginFile: VITE_APP_BUILD_COMPRESS_DELETE_ORIGIN_FILE // 源文件压缩后是否删除
-				})
-			);
-		law === "brotli" &&
-			plugins.push(
-				viteCompression({
-					verbose: true, // 是否在控制台中输出压缩结果
-					disable: false,
-					threshold: 1024, // 如果体积大于阈值，将被压缩，单位为b，体积过小时请不要压缩，以免适得其反
-					algorithm: "brotliCompress", // 压缩算法
-					ext: ".br",
-					deleteOriginFile: VITE_APP_BUILD_COMPRESS_DELETE_ORIGIN_FILE // 源文件压缩后是否删除
-				})
-			);
-	}
-	return plugins;
+  const { VITE_APP_BUILD_COMPRESS, VITE_APP_BUILD_COMPRESS_DELETE_ORIGIN_FILE } = viteEnv;
+  const compressList = VITE_APP_BUILD_COMPRESS.split(",");
+  const plugins: PluginOption[] = [];
+  for (const law of compressList) {
+    law === "gzip" &&
+      plugins.push(
+        viteCompression({
+          verbose: true, // 是否在控制台中输出压缩结果
+          disable: false,
+          threshold: 1024, // 如果体积大于阈值，将被压缩，单位为b，体积过小时请不要压缩，以免适得其反
+          algorithm: "gzip", // 压缩算法
+          ext: ".gz",
+          deleteOriginFile: VITE_APP_BUILD_COMPRESS_DELETE_ORIGIN_FILE // 源文件压缩后是否删除
+        })
+      );
+    law === "brotli" &&
+      plugins.push(
+        viteCompression({
+          verbose: true, // 是否在控制台中输出压缩结果
+          disable: false,
+          threshold: 1024, // 如果体积大于阈值，将被压缩，单位为b，体积过小时请不要压缩，以免适得其反
+          algorithm: "brotliCompress", // 压缩算法
+          ext: ".br",
+          deleteOriginFile: VITE_APP_BUILD_COMPRESS_DELETE_ORIGIN_FILE // 源文件压缩后是否删除
+        })
+      );
+  }
+  return plugins;
 };
