38745d7429f2f3b725dc09519bfbc602d0b14e3b
Symbol <1783176286@qq.com>
2023-05-22 18:34:37 +0800

    types: 类型优化

--- src/api/modules/auth.ts
+++ src/api/modules/auth.ts
@@ -1,18 +1,11 @@
 import request from "@/api";
+import { ResponseData } from "../interface/index";
 // import { Auth } from "../interface/index";
-// import menuList from "@/config/menuList.json";
+import menuList from "@/config/menuList.json";
 
 // 获取菜单/路由
-export const menuListApi = () => {
+export const menuListApi = (): Promise<ResponseData<Menu.MenuOptions[]>> => {
   return request.get<Menu.MenuOptions[]>(`/menu/list`);
   // 本地数据
-  // return new Promise((resolve, reject) => {
-  // 	setTimeout(() => {
-  // 		if (params) {
-  // 			resolve(menuList);
-  // 		} else {
-  // 			resolve(menuList);
-  // 		}
-  // 	}, 500);
-  // });
+  return Promise.resolve(menuList);
 };
